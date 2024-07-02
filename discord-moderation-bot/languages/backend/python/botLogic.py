cd80575bf928b667d50dec75d4b1c79c321cb58c
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-07-02 12:29:39 +0530

    Generated: `discord-moderation-bot/languages/backend/python/botLogic.py`

---
+++ discord-moderation-bot/languages/backend/python/botLogic.py
@@ -0,0 +1,15 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} has connected to Discord!')
+
+@bot.command(name='ping')
+async def ping(ctx):
+    await ctx.send('Pong!')
+
+bot.run('YOUR_DISCORD_BOT_TOKEN')
